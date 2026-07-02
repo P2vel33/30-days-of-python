577095e662b50b0978b6fa5796fcf18fe99f0850
Pavel Fedotov <samasyt@mail.ru>
2026-07-02 13:36:12 +0300

    first commit

---
+++ day-1/hello.py
@@ -0,0 +1,25 @@
+# exexercise 1
+print(3 + 4)
+print(3 * 4) 
+print(3 ** 4) 
+print(5 - 3) 
+print(5 / 3) 
+print(5 % 3) 
+print(5 // 3) 
+
+# exexercise 2
+print(type([1,2,3]))
+print(type('string'))
+print(type(1))
+print(type(1.3))
+print(type(3.14))
+print(type(1.3 + 1j))
+print(type(('a','b','c')))
+print(type({'a','b','c'}))
+print(type({'a': 'hello', "b":"bye"}))
+
+# exexercise 3
+import math;
+p = (2, 3)
+y = (10, 8)
+print(math.sqrt((p[0] - y[0]) ** 2 + (p[1] - y[1]) ** 2))

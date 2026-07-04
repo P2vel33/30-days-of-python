2b83a33e52e60835b3aad4a37daef871fd0a9c09
Pavel Fedotov <samasyt@mail.ru>
2026-07-04 23:49:47 +0300

    upd: day-3

---
+++ day-3/day-3.py
@@ -0,0 +1,28 @@
+# exexercise 3.1
+developer_age:int = 24
+
+# exexercise 3.2
+developer_height:float = 1.78
+
+# exexercise 3.3
+complex_var:complex = 1.78 + 1j
+
+# exexercise 3.4
+triangle_base = input("Enter base: ")
+triangle_height = input("Enter height: ")
+area = float(triangle_base) * float(triangle_height) / 2
+print("The area of the triangle is " + str(area))
+
+# exexercise 3.5
+triangle_a = input("Enter triangle_a: ")
+triangle_b = input("Enter triangle_b: ")
+triangle_c = input("Enter triangle_c: ")
+perimeter  = float(triangle_a) + float(triangle_b) + float(triangle_c);
+print("The perimeter of the triangle is " + str(perimeter))
+
+# exexercise 3.6
+rectangle_height = input("Enter rectangle_height: ")
+rectangle_width = input("Enter rectangle_width: ")
+perimeter  = float(triangle_a) + float(triangle_b) + float(triangle_c);
+print("The perimeter of the triangle is " + str(perimeter))
+

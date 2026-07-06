0deca313088b8ff4783c2a8d6a352a6d0f9538bc
Pavel Fedotov <samasyt@mail.ru>
2026-07-06 17:53:57 +0300

    upd: day-4

---
+++ day-4/string.py
@@ -0,0 +1,88 @@
+# exexercise 4.1
+print("Thirty" + " " + "Days"  + " " + "Of" + " " + "Python")
+
+# exexercise 4.2
+coding42 = "Coding"
+for42 = "For"
+all42 = "All"
+space = " "
+print(coding42 + space + for42 + space + all42)
+
+# exexercise 4.3
+company43 = "Coding For All" 
+
+# exexercise 4.4
+print(company43)
+
+# exexercise 4.5
+print(len(company43))
+
+# exexercise 4.6
+print(company43.upper())
+
+# exexercise 4.7
+print(company43.lower())
+
+# exexercise 4.8
+print(company43.capitalize())
+print(company43.title())
+print(company43.swapcase())
+
+# exexercise 4.9
+print(company43[7:])
+
+# exexercise 4.10
+print(company43.index('Coding'))
+print(company43.find('Coding'))
+print('Coding' in company43)
+
+# exexercise 4.11
+print(company43.replace("Coding","Python"))
+
+# exexercise 4.12
+print("Python for Everyone".replace("Everyone","All"))
+
+# exexercise 4.13
+print(company43.split(" "))
+
+# exexercise 4.14
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(","))
+
+# exexercise 4.15
+print(company43[0])
+print(company43[0:1])
+
+# exexercise 4.16
+print(len(company43) - 1)
+print(company43[len(company43) - 1])
+
+# exexercise 4.17
+print(company43[10])
+
+# exexercise 4.18
+abbreviation418 = company43.replace("Coding", "Python").split()
+print(abbreviation418[0][0] + abbreviation418[1][0] + abbreviation418[2][0])
+
+# exexercise 4.19
+abbreviation419 = company43.split()
+print(abbreviation419[0][0] + abbreviation419[1][0] + abbreviation419[2][0])
+
+# exexercise 4.20
+print("Coding For All".index("C"))
+
+# exexercise 4.21
+print("Coding For All".index("F"))
+
+# exexercise 4.22
+print("Coding For All People".rfind("l"))
+
+# exexercise 4.23
+print("You cannot end a sentence with because because because is a conjunction".find("because"))
+
+# exexercise 4.24
+print("You cannot end a sentence with because because because is a conjunction".rindex("because"))
+
+# exexercise 4.25
+print("You cannot end a sentence with because because because is a conjunction".replace(" because because because", ""))
+
+

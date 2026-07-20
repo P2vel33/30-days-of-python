c08b9c2d9dd93a3a4b6bdef3050bdf7f3e802739
Pavel Fedotov <samasyt@mail.ru>
2026-07-20 14:18:06 +0300

    upd: ended day-9

--- day-9/conditionals.py
+++ day-9/conditionals.py
@@ -18,3 +18,82 @@ elif a < b:
     print("{} is less than {}".format(a,b))
 else:
     print("{} is equals than {}".format(a,b))
+    
+# day 9 level 2 exexercise 1
+points = int(input("Your points: "))
+if points >= 90:
+    print("Your grade: A")
+elif points >= 80:
+    print("Your grade: B")
+elif points >= 70:
+    print("Your grade: C")
+elif points >= 60:
+    print("Your grade: D")
+else:
+    print("Your grade: F")
+    
+# day 9 level 2 exexercise 2
+month = input("Month: ")
+month = month.strip()
+if month in ["September", "October","November"]:
+    print("Autumn")
+elif month in ["December", "January","February"]:
+    print("Winter")
+elif month in ["March", "April","May"]:
+    print("Spring")
+elif month in ["June", "July","August"]:
+    print("Summer")
+else:
+    print("You entered a non-existent month")
+    
+    
+# day 9 level 2 exexercise 3
+fruits = ['banana', 'orange', 'mango', 'lemon']
+new_friuts = input("Fruit: ")
+if(new_friuts in fruits):
+    print("That fruit already exist in the list")
+else:
+    fruits.append(new_friuts)
+    print(fruits)
+    
+# day 9 level 3 exexercise 1
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+if "skills" in person and type(person['skills'] == "list"):
+    print(person['skills'][len(person["skills"]) // 2])
+else:
+    print("skills not found")
+
+# day 9 level 3 exexercise 2
+if "skills" in person:
+    print("Python" in person['skills'])
+else:
+    print("skills not found")
+
+# day 9 level 3 exexercise 3
+if "skills" in person:
+    if set(["React", "Node", "MongoDB"]).issubset(set(person["skills"])):
+        print("He is a fullstack developer")
+    elif set(["Node", "Python", "MongoDB"]).issubset(set(person["skills"])):
+        print("He is a backend developer")
+    elif set(['JavaScript', 'React']).issubset(set(person["skills"])):
+        print("He is a frontend developer")
+    else:
+        print("He isn`t developer")
+else:
+    print("skills not found")
+
+# day 9 level 3 exexercise 4
+if person["is_married"] == True and person['country'] == 'Finland':
+    print("{} {} lives in Finland. He is married.".format(person['first_name'],person['last_name']))
+

ee48eeb379a22e8e985504db06caec71a3f054dd
Pavel Fedotov <samasyt@mail.ru>
2026-08-04 17:53:03 +0300

    upd: day-11 ( level-2 ) ( 3 exer )

--- day-11/functions.py
+++ day-11/functions.py
@@ -93,5 +93,93 @@ def reverse_list (example_list):
             result.insert(0,item_of_list)
         return result
     else:
-        return print("The data entered is not a list!")
+        return "The data entered is not a list!"
 print(reverse_list([1,2,3,4]))
+
+# day 11 level 1 exexercise 10
+print("\nday 11 level 1 exexercise 10")
+def capitalize_list_items (example_list):
+    if (type(example_list)) == list:
+        result = []
+        for item_of_list in example_list:
+            if(type(item_of_list) == str):
+                item_of_list = item_of_list.capitalize()
+                result.insert(0,item_of_list)
+            else:
+                return "Not all list inputs are strings!"
+        return result
+    else:
+        return print("The data entered is not a list!")
+print(capitalize_list_items(['all','python']))
+
+# day 11 level 1 exexercise 11
+print("\nday 11 level 1 exexercise 11")
+def add_item (example_list, new_item):
+    if (type(example_list)) == list:
+        example_list.append(new_item)
+        return example_list
+    else:
+        return print("The data entered is not a list!")
+print(add_item(['All', 'likes'],'python'))
+
+# day 11 level 1 exexercise 12
+print("\nday 11 level 1 exexercise 12")
+def remove_item (example_list, rm_item):
+    if (type(example_list)) == list:
+        if(rm_item in example_list):
+            example_list.remove(rm_item)
+            return example_list
+        else:
+            return "This value is not in the list!"
+    else:
+        return print("The data entered is not a list!")
+print(remove_item(['All', 'likes', 'python'],'python'))
+
+# day 11 level 1 exexercise 13
+print("\nday 11 level 1 exexercise 13")
+def sum_of_numbers (number):
+    if type(number) == int:
+        result = 0
+        for i in range(number):
+            result += i
+        return result + number
+    else:
+        return "The input value is not a number"
+print(sum_of_numbers(3))
+
+# day 11 level 2 exexercise 1
+print("\nday 11 level 2 exexercise 1")
+def evens_and_odds (number):
+    if type(number) == int and number > 0:
+        result_odd = 0
+        result_evens = 0
+        for i in range(number+1):
+            if(i % 2 == 0):
+                result_odd += 1
+            else:
+                result_evens += 1
+        return "The number of odds are {}. The number of evens are {}.".format(result_odd, result_evens) 
+    else:
+        return "The input value is not a number"
+print(evens_and_odds(3))
+
+# day 11 level 2 exexercise 2
+print("\nday 11 level 2 exexercise 2")
+def factorial (number):
+    if type(number) == int and number > 0:
+        result = 1
+        for i in range(1,number+1):
+            result *= i
+        return result
+    else:
+        return "The input value is not a number"
+print(factorial(4))
+
+# day 11 level 2 exexercise 3
+print("\nday 11 level 2 exexercise 3")
+def is_empty (param):
+    if param:
+        return False
+    else:
+        return True
+print(is_empty(4))

aa42854a53f94d25210b3639447a10ec49e4a9a5
Pavel Fedotov <samasyt@mail.ru>
2026-08-18 16:37:59 +0300

    upd: day-14 ( level 1 )

---
+++ day-14/high-order-functions.py
@@ -0,0 +1,23 @@
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# day 14 level 1 exexercise 3
+print("\nday 14 level 1 exexercise 3")
+from functools import reduce
+
+# day 14 level 1 exexercise 4
+print("\nday 14 level 1 exexercise 4")
+for country in countries:
+    print(country)
+
+# day 14 level 1 exexercise 5
+print("\nday 14 level 1 exexercise 5")
+for name in names:
+    print(name)
+
+# day 14 level 1 exexercise 6
+print("\nday 14 level 1 exexercise 6")
+for number in numbers:
+    print(number)
